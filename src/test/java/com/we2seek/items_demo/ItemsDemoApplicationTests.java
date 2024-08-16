61d1d45fddd27c70fef1166ba310bd43d7e9928b
Vitaliy Timchenko <vitaliy.timchenko@privatbank.ua>
2024-08-16 20:32:35 +0300

    add integration test: mvn verify

--- src/test/java/com/we2seek/items_demo/ItemsDemoApplicationTests.java
+++ src/test/java/com/we2seek/items_demo/ItemsDemoApplicationTests.java
@@ -3,10 +3,10 @@ package com.we2seek.items_demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ItemsDemoApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
