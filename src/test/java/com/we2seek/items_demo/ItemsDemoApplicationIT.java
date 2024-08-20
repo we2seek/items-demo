04fed1a3c396ac86a0c7ae46f2c3f9c70ec1e158
Vitaliy Timchenko <vitaliy.timchenko@privatbank.ua>
2024-08-20 13:32:58 +0300

    update integration test

--- src/test/java/com/we2seek/items_demo/ItemsDemoApplicationIT.java
+++ src/test/java/com/we2seek/items_demo/ItemsDemoApplicationIT.java
@@ -1,6 +1,5 @@
 package com.we2seek.items_demo;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -14,34 +13,28 @@ public class ItemsDemoApplicationIT {
     private static final char FILL_CHAR = '.';
     private static final char[] DOTS = new char[10];
 
-    @BeforeEach
-    public void setUp() {
-        Arrays.fill(DOTS, EMPTY_CHAR);
-    }
-
     @Test
     public void someFakeTestOne() throws InterruptedException {
-        for (int i = 0; i < DOTS.length; i++) {
-            for (int j = 0; j <= i; j++) {
-                DOTS[j] = FILL_CHAR;
-            }
-            System.out.println("Test #1" + new String(DOTS));
-            Thread.sleep(System.currentTimeMillis() % 1000);
-        }
-
+        showDummyText("Test #1");
         Thread.sleep(1000);
     }
 
     @Test
     public void someFakeTestTwo() throws InterruptedException {
+        showDummyText("Test #2");
+        Thread.sleep(1000);
+    }
+
+    private static void showDummyText(String prefix) throws InterruptedException {
+        StringBuilder sb = new StringBuilder(prefix);
+        Arrays.fill(DOTS, EMPTY_CHAR);
         for (int i = 0; i < DOTS.length; i++) {
             for (int j = 0; j <= i; j++) {
                 DOTS[j] = FILL_CHAR;
             }
-            System.out.println("Test #2" + new String(DOTS));
-            Thread.sleep(System.currentTimeMillis() % 1000);
+            System.out.println(sb.append(' ').append(DOTS));
+            sb.setLength(prefix.length());
+            Thread.sleep(System.currentTimeMillis() % 500);
         }
-
-        Thread.sleep(1000);
     }
 }
