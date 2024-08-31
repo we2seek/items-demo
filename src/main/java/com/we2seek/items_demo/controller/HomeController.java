a1bae5a728141dd4cda7dd1d39507a22f92cc959
RomanEvgenovichLoboda <loboda.rom@gmail.com>
2024-08-31 21:02:10 +0300

    use thymeleaf fragments

--- src/main/java/com/we2seek/items_demo/controller/HomeController.java
+++ src/main/java/com/we2seek/items_demo/controller/HomeController.java
@@ -5,10 +5,13 @@ import com.we2seek.items_demo.converter.ItemFormToItemConverter;
 import com.we2seek.items_demo.converter.ItemToItemFromConverter;
 import com.we2seek.items_demo.dao.ItemDAO;
 import com.we2seek.items_demo.domain.Item;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -17,6 +20,7 @@ import java.util.stream.Collectors;
 @Controller
 public class HomeController {
 
+    private static final Logger log = LoggerFactory.getLogger(HomeController.class);
     private final ItemDAO itemDAO;
     private final ItemFormToItemConverter itemFormToItemConverter;
     private final ItemToItemFromConverter itemToItemFromConverter;
@@ -25,6 +29,12 @@ public class HomeController {
         this.itemDAO = itemDAO;
         this.itemFormToItemConverter = itemFormToItemConverter;
         this.itemToItemFromConverter = itemToItemFromConverter;
+    }
+
+    private void getLog(Model model) {
+        log.info("Title = {}", model.getAttribute("title"));
+        log.info("Header = {}", model.getAttribute("header"));
+        log.info("Fragment = {}", model.getAttribute("fragment"));
     }
 
     @GetMapping({"/"})
@@ -36,19 +46,31 @@ public class HomeController {
                         .collect(Collectors.toList())
         );
 
+        model.addAttribute("title", "Items");
+        model.addAttribute("header", "Application main page");
+        model.addAttribute("fragment", "tableItems");
+
+        getLog(model);
+
         return "index";
     }
 
-    @GetMapping(value = "/items/create")
+    @GetMapping(value = "/create")
     public String showCreateForm(Model model) {
         ItemForm itemForm = new ItemForm();
         itemForm.setActive(true);
         model.addAttribute("itemForm", itemForm);
+        model.addAttribute("title", "Create");
 
-        return "item/form";
+        model.addAttribute("header", "Create item");
+        model.addAttribute("fragment", "editForm");
+
+        getLog(model);
+
+        return "index";
     }
 
-    @PostMapping("/items/create")
+    @PostMapping("/create")
     public String saveItem(ItemForm form, BindingResult bindingResult) {
         Item item = itemFormToItemConverter.convert(form);
         itemDAO.create(item);
@@ -56,32 +78,44 @@ public class HomeController {
         return "redirect:/";
     }
 
-    @GetMapping("/items/edit/{id}")
+    @GetMapping("/{id}")
     public String showEditForm(@PathVariable Integer id, Model model) {
         Item item = itemDAO.get(id);
         ItemForm itemForm = itemToItemFromConverter.convert(item);
         model.addAttribute("itemForm", itemForm);
 
-        return "item/form";
+        model.addAttribute("title", "Edit");
+        model.addAttribute("header", "Edit item");
+        model.addAttribute("fragment", "editForm");
+
+        getLog(model);
+
+        return "index";
     }
 
-    @GetMapping("/items/delete/{id}")
+    @GetMapping("/delete/{id}")
     public String showDeleteForm(@PathVariable Integer id, Model model) {
         Item item = itemDAO.get(id);
         ItemForm itemForm = itemToItemFromConverter.convert(item);
         model.addAttribute("itemForm", itemForm);
 
-        return "item/deleteForm";
+        model.addAttribute("title", "Delete");
+        model.addAttribute("header", "Delete item");
+        model.addAttribute("fragment", "deleteForm");
+
+        getLog(model);
+
+        return "index";
     }
 
-    @PostMapping(value = "/items/delete", params = "action=delete")
+    @PostMapping(value = "/delete", params = "action=delete")
     public String deleteItem(ItemForm form) {
         itemDAO.delete(form.getId());
 
         return "redirect:/";
     }
 
-    @PostMapping(value = "/items/delete", params = "action=cancel")
+    @PostMapping(value = "/delete", params = "action=cancel")
     public String deleteItemCancel() {
         return "redirect:/";
     }
